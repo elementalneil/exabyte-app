ee91d7991c36c7bd60dea24bad9facd4ce2a6f27
elementalneil <nayek.sankhanil@gmail.com>
2022-11-07 16:50:11 +0530

    Added database handling scripts for adding events
    - Created new classes to handle database operations for events
    - Changed font throughout the website to Anonymous Pro

---
+++ event_operations.py
@@ -0,0 +1,60 @@
+import sqlite3
+from datetime import date, time
+
+class Events:
+    __db_connection = None
+    __db_cursor = None
+
+    def __init__(self):
+        self.__db_connection = sqlite3.connect("EventDB.sqlite")
+        self.__db_cursor = self.__db_connection.cursor()
+
+        initscript = '''
+            CREATE TABLE IF NOT EXISTS Events (
+                EventID integer NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
+                EventName varchar2(255) NOT NULL,
+                Venue varchar2(63),
+                Date date NOT NULL,
+                Time time,
+                Contact_name varchar2(255),
+                Contact_no varchar2(20)
+            );
+        '''
+        
+        self.__db_cursor.executescript(initscript)
+
+    def create_event(self, event_args):     # event_args is a dictionary containing event attributes
+        query = '''INSERT INTO 
+                   Events(EventName, Venue, Date, Time, Contact_name, Contact_no)
+                   VALUES (?, ?, ?, ?, ?, ?)'''
+        self.__db_cursor.execute(query, (event_args['name'],
+                                         event_args['venue'],
+                                         event_args['date'],
+                                         event_args['time'],
+                                         event_args['contact_name'],
+                                         event_args['contact_num']) )
+        self.__db_connection.commit()
+
+    def view_events(self):
+        query = 'SELECT * FROM Events'
+        self.__db_cursor.execute(query)
+        print(self.__db_cursor.fetchall())
+        
+
+def main():
+    event_obj = Events()
+
+    args = {}
+    args['name'] = 'Proshow1'
+    args['venue'] = 'Football Ground'
+    args['date'] = date(2022, 10, 25)
+    args['time'] = '07:30:00'
+    args['contact_name'] = 'John Doe'
+    args['contact_num'] = '+91 815 568 9973'
+
+    event_obj.create_event(args)
+
+    event_obj.view_events()
+
+if __name__ == '__main__':
+    main()
--- main_ui.py
+++ main_ui.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request, session
 from flask import redirect, url_for
+from datetime import date, time
 import login_operations as login_op
+from event_operations import Events
 
 app = Flask(__name__)
 
@@ -56,3 +58,42 @@ def logout():
     # Deletes an existing session
     session.pop('username', None)
     return redirect(url_for('index'))
+
+
+@app.route('/add_event', methods = ['POST', 'GET'])
+def create_event():
+    if 'username' not in session:
+        return redirect(url_for('index'))
+
+    event_obj = Events()
+
+    args = {}
+    if request.method == 'POST':
+        args['name'] = request.form['event_name']
+        args['venue'] = request.form['event_venue']
+
+        date_ = request.form['event_date']
+        time_ = request.form['event_time']
+        args['date'] = date(int(date_[0:4]), int(date_[5:7]), int(date_[8:10]))
+        args['time'] = time_ + ':00'
+    
+        args['contact_name'] = request.form['contact_name']
+        args['contact_num'] = request.form['contact_num']
+
+        event_obj.create_event(args)
+
+    # args = {}
+    # # if request.method == 'POST':
+    # args['name'] = 'Proshow2'
+    # args['venue'] = 'Football Ground'
+    # args['date'] = date(2022, 10, 25)
+    # args['time'] = '07:30:00'
+    # args['contact_name'] = 'John Doe'
+    # args['contact_num'] = '+91 815 568 9973'
+
+    return render_template('addEvent.html')
+
+
+@app.route('/event')
+def event():
+    return render_template('event.html')
