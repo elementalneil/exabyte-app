e09a60b919713e35fc323423f34651d973e53204
elementalneil <nayek.sankhanil@gmail.com>
2022-11-25 10:42:10 +0530

    Created Faculty DB Operations
    - Create Operations are completed
    - Will implement modify, delete operations subsequently

--- participant_ops.py
+++ participant_ops.py
@@ -52,8 +52,8 @@ class Student(Participant):
 
         flag = 1  
         # 0 represents no student exists. 
-        # 1 represents student exists, but no event. 
-        # 2 represents student and event both exist
+        # 1 represents student exists, but is not currently participating in given event.
+        # 2 represents student exists and is already registered in given event.
         
         self._db_cursor.execute('SELECT PID FROM ID_Student WHERE Roll_no = ?', (roll_no, ))
 
@@ -61,7 +61,7 @@ class Student(Participant):
         if(pid == None):
             flag = 0
         else:
-            self._db_cursor.execute('SELECT Event_ID FROM Participants WHERE PID = ?', (pid[0], ))
+            self._db_cursor.execute('SELECT EventID FROM Participants WHERE PID = ?', (pid[0], ))
             for row in self._db_cursor.fetchall():
                 if row[0] == event_id:
                     flag = 2
@@ -92,6 +92,7 @@ class Student(Participant):
                                             student_args['sem']))
             
             self._db_connection.commit()
+            return True
 
         else:
             self._db_cursor.execute('SELECT PID FROM ID_Student WHERE Roll_no = ?', (student_args['roll_no']))
@@ -100,6 +101,7 @@ class Student(Participant):
             self._db_cursor.execute('INSERT INTO Participants (PID, Event_ID) VALUES (?, ?)', pid, event_id)
 
             self._db_connection.commit()
+            return True
         
 
 
@@ -122,6 +124,60 @@ class Faculty(Participant):
         '''
 
         self._db_cursor.executescript(initscript)
+
+    def register_faculty(self, faculty_args, event_id):
+        fid = faculty_args['fid'].upper()
+
+        flag = 1
+        # 0 represents no faculty exists. 
+        # 1 represents faculty exists, but is not currently participating in given event.
+        # 2 represents faculty exists and is already registered in given event.
+        
+        self._db_cursor.execute('SELECT PID FROM ID_Faculty WHERE F_ID = ?', (fid, ))
+
+        pid = self._db_cursor.fetchone()
+        if(pid == None):
+            flag = 0
+        else:
+            self._db_cursor.execute('SELECT EventID FROM Participants WHERE PID = ?', (pid[0], ))
+            for row in self._db_cursor.fetchall():
+                if row[0] == event_id:
+                    flag = 2
+                    break
+
+        if flag == 2:
+            return False
+
+        elif flag == 0:
+            query = 'INSERT INTO Participant_Type (Ptype) VALUES (2)'
+            self._db_cursor.execute(query)
+            self._db_cursor.execute('SELECT MAX(PID) FROM Participant_Type')
+            pid = self._db_cursor.fetchone()[0]
+
+            self._db_cursor.execute('INSERT INTO Participants (PID, EventID) VALUES (?, ?)', (pid, event_id, ))
+
+            self._db_cursor.execute('INSERT INTO ID_Faculty (PID, F_ID) VALUES (?, ?)', (pid, faculty_args['fid'], ))
+            
+            query = '''
+                INSERT INTO Faculty (F_ID, Name, Contact_num, Department)
+                VALUES (?, ?, ?, ?)
+            '''
+            self._db_cursor.execute(query, (faculty_args['fid'],
+                                            faculty_args['name'],
+                                            faculty_args['contact_no'],
+                                            faculty_args['dept']))
+            
+            self._db_connection.commit()
+            return True
+
+        else:
+            self._db_cursor.execute('SELECT PID FROM ID_Student WHERE Roll_no = ?', (faculty_args['fid']))
+            pid = self._db_cursor.fetchone()[0]
+
+            self._db_cursor.execute('INSERT INTO Participants (PID, Event_ID) VALUES (?, ?)', pid, event_id)
+
+            self._db_connection.commit()
+            return True
 
 
 class Outsider(Participant):
@@ -147,17 +203,19 @@ class Outsider(Participant):
 
 
 def main():
-    std = Student()
+    std = Faculty()
 
     args = {}
-    args['roll_no'] = 'M210708CA'
-    args['name'] = 'Golu Singh'
-    args['contact_no'] = '1235466878'
-    args['email'] = 'golugolu@gmail.com'
+    args['fid'] = '5633CS'
+    args['name'] = 'Dr. Jayaraj PB'
+    args['contact_no'] = '38349 12321'
     args['dept'] = "CSED"
-    args['sem'] = 3
 
-    std.register_student(args, 3)
+    flag = std.register_faculty(args, 1)
+    if flag:
+        print('Registered Successfully')
+    else:
+        print('You are already registered')
 
 if __name__ == '__main__':
     main()
